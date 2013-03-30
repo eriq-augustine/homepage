fb3fa6f2704a79be287528f9e0b4dee9652dcb33
Eriq Augustine <eaugusti@chromium.org>
2013-03-29 22:30:16 -0700

    Updated about page.

--- about.php
+++ about.php
@@ -11,10 +11,10 @@
          <div id="main">
             <div class="main-content">
                <h2>About Me</h2>
-               <p>My name is Eriq Augustine. I am a 4th year graduate Computer Science student at Cal Poly, San Luis Obispo. I hail from Los Gatos, California.</p>
-               <p>My technical interests include Natural Language Processing (NLP), Datamining, and Compilers. You can see my <a href="docs/resume.pdf">resume</a> and <a href="projects">projects page</a> for a more complete listing of my technical abilities/interests.</p>
-               <p>My hobbies include video games, anime, <a href="cases">case building</a>, badminton, and learning languages. I am currently working on my Japanese and Chinese (Mandrin). I hope to expand into either Cantonese or Korean this summer.</p>
-               <p>I don't have a facebook, but feel free to email me at eaugusti[at]calpoly.edu. You can also check me out on <a href="http://www.linkedin.com/pub/eriq-augustine/21/539/771">Linkedin</a></p>
+               <p>My name is Eriq Augustine. I am a Cal Poly Computer Science graduate. I hail from Los Gatos, California.</p>
+               <p>My technical interests include Natural Language Processing (NLP), Datamining, Databases, and Compilers. You can see my <a href="files/resume.pdf">resume</a> and <a href="projects">projects page</a> for a more complete listing of my technical abilities/interests.</p>
+               <p>My hobbies include video games, anime, <a href="cases">case building</a>, badminton, and learning languages. I am currently working on my Japanese and Chinese (Mandrin). I hope to expand into either Korean or Russian next.</p>
+               <p>You can also check me out on <a href="http://www.linkedin.com/pub/eriq-augustine/21/539/771">Linkedin</a></p>
             </div>
          </div>
 
