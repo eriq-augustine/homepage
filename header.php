0543e3f10653aac8c53f42532cff74a0c0bf6f8a
Eriq Augustine <eaugusti@chromium.org>
2013-03-29 22:31:35 -0700

    Changed email to gmail.

--- header.php
+++ header.php
@@ -2,6 +2,6 @@
    <div id="title">
       <h1><a href=<?php echo "\"$webBase/\"" ?>>Eriq Augustine</a></h1>
       <p class="slogan">Computer Scientist</p>
-      <p class="subslogan">eaugusti[at]calpoly.edu</p>
+      <p class="subslogan">eriq.public[at]gmail.com</p>
    </div>
 </div>
