0b0602ab4273638e22ec3c6cb0cccfaca74a4624
Eriq Augustine <eaugusti@chromium.org>
2016-01-02 10:42:26 -0800

    Added the base 102 page.

--- sidebar.php
+++ sidebar.php
@@ -4,6 +4,7 @@
          $menuItems = array(
             array('/', 'main', 'Main'),
             array('/cpe101-winter14', 'cpe101', 'CPE 101'),
+            array('/cpe102-winter16', 'cpe102', 'CPE 102'),
             array('/cpe105', 'cpe105', 'CPE 105'),
             array('/csc141-spring15', 'csc141', 'CSC 141'),
             array('/cpe365', 'cpe365', 'CPE 365'),
