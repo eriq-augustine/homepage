472f732a2899dc93f1ac1ce9df608ab8a1eeec24
Eriq Augustine <eaugusti@chromium.org>
2016-09-27 14:15:37 -0700

    Fixed a bug in the index.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
    <div class="main-content">
       <h2 class='page-title'>Welcome!</h2>
       <p>Hi there. I am Eriq Augustine and this is my personal website.</p>
-      <p>Feel free to look around. For more information about me check out my <a href="<?php absLink('about'); ?>">About Section.</a></p>
+      <p>Feel free to look around. For more information about me check out my <a href="<?php echo absLink('pages/about'); ?>">About Section.</a></p>
    </div>
 </div>
 
